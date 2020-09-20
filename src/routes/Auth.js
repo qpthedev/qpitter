b394499f32b8eed9aa28e8b4fb8dab699826bec9
qpthedev <qpthedev@gmail.com>
2020-09-20 04:01:01 -0400

    social login with google and github

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
@@ -31,13 +31,29 @@ const Auth = () => {
         // log in
         data = await authService.signInWithEmailAndPassword(email, password);
       }
-      console.log(data);
+      // console.log(data);
     } catch (error) {
       setError(error.message);
     }
   };
 
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+
+    let provider;
+
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+
+    console.log(data);
+  };
 
   return (
     <div>
@@ -70,8 +86,12 @@ const Auth = () => {
         </span>
       </div>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
