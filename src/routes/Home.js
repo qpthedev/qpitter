193302a8736418101435990231725fbbcbfd101b
qpthedev <qpthedev@gmail.com>
2020-09-20 04:55:20 -0400

    fetch qweets collections to home screen

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [qweet, setQweet] = useState("");
+  const [qweets, setQweets] = useState([]);
+  const getQweets = async () => {
+    const dbQweets = await dbService
+      .collection("qweets")
+      .orderBy("createdAt", "asc")
+      .get();
+    dbQweets.forEach((document) => {
+      const qweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setQweets((prev) => [qweetObject, ...prev]);
+    });
+  };
 
-  const onSubmit = (event) => {
+  useEffect(() => {
+    getQweets();
+  }, []);
+
+  const onSubmit = async (event) => {
     event.preventDefault();
+    await dbService.collection("qweets").add({
+      qweet,
+      createdAt: Date.now(),
+    });
+    setQweet("");
   };
 
   const onChange = (event) => {
@@ -27,6 +51,13 @@ const Home = () => {
         />
         <input type="submit" value="QWEET" />
       </form>
+      <div>
+        {qweets.map((qweet) => (
+          <div key={qweet.id}>
+            <h4>{qweet.qweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
